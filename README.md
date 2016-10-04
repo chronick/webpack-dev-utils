f7ee729060c56a994edc36397fc8f006a8fdf2ec
headcanon <ndonohue@gmail.com>
2016-10-04 12:52:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Webpack Dev Utils
+
+A set of utilities for webpack development.
+Many of these were pulled from [`react-dev-utils`](https://www.npmjs.com/package/react-dev-utils)
+
+## Installation
+
+```
+npm install --save-dev webpack-dev-utils
+```
---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  checkRequiredFiles: require('./check_required_files'),
+  formatWebpackMessages: require('./format_webpack_messages'),
+  InterpolateHtmlPlugin: require('./interpolate_html_plugin'),
+  WatchMissingNodeModulesPlugin: require('./watch_missing_node_modules_plugin')
+}
